a95d397e03f0c99b7c2b7cc10e60b6aa776289bf
bala reddy <balagangadhar864@gmail.com>
2026-08-12 07:23:38 +0000

    55

--- models/example/staging/dim_customers.sql
+++ models/example/staging/dim_customers.sql
@@ -1,12 +1,3 @@
 {{ config(materialized='view') }}
 
 select * from {{ ref('stg_customers') }}
-
-dbt run --select stg_customers+
-
-
-{{ config(materialized='table') }}
-
-select * from {{ ref('stg_customers') }}
-
-dbt run --select stg_customers+
