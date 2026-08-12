421a1f7e1e43accd9bced1c5fcca3d0a6b6fb15c
bala reddy <balagangadhar864@gmail.com>
2026-08-12 10:10:58 +0000

    dd

---
+++ snapshots/customers_snapshot.sql
@@ -0,0 +1,18 @@
+{% snapshot customers_snapshot %}
+{{
+    config(
+        target_schema='SNAPSHOTS',
+        unique_key='customer_id',
+        strategy='timestamp',
+        updated_at='updated_date'
+    )
+}}
+SELECT
+    customer_id,
+    customer_name,
+    city,
+    age,
+    updated_date
+FROM {{ source('raw', 'customers') }}
+{% endsnapshot %}
+
